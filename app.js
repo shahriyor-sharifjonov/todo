452a924658f02b0abe1e372ade02fdf55817b27e
Shahriyor <shahriyordeveloping@gmail.com>
2021-06-08 18:18:31 +0300

    initial

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function addTodo(event){
   //CHECK TRASH BUTTON
   const trashButton = document.createElement('button');
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("complete-btn");
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   //APPEND TO LIST
   todoList.appendChild(todoDiv);
